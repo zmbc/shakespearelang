f82998254b5b93c6fafc36fa23fa4c1566d42125
Zeb Burke-Conte <zmbc@uw.edu>
2016-03-19 19:30:32 -0700

    Add interpreter stump

---
+++ shakespeare_interpreter.py
@@ -0,0 +1,15 @@
+from shakespeare_parser import shakespeareParser
+import argparse
+
+argparser = argparse.ArgumentParser(description = "Run files in Shakespeare Programming Language.")
+argparser.add_argument('filename', type=str, help="SPL file location")
+
+args = argparser.parse_args()
+
+if(args.filename):
+    with open(args.filename, 'r') as f:
+        text = f.read().replace('\n', ' ')
+
+    parser = shakespeareParser()
+    ast = parser.parse(text, rule_name='play')
+    print(ast)
