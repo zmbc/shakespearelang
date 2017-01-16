12bb0b06af7e44ab041a1d23c22b09db6dc4b0bf
Zeb Burke-Conte <zmbc@uw.edu>
2017-01-15 21:44:48 -0800

    add more blank lines in cli.py

--- shakespearelang/cli.py
+++ shakespearelang/cli.py
@@ -4,15 +4,18 @@ import click
 from .shakespeare_interpreter import Shakespeare
 from .repl import start_console, debug_play
 
+
 @click.group(invoke_without_command=True)
 @click.pass_context
 def main(ctx):
     if ctx.invoked_subcommand is None:
         console()
 
+
 @main.command()
 def console():
     start_console()
+
 
 @main.command()
 @click.argument('file')
@@ -21,6 +24,7 @@ def run(file):
         play = f.read()
         interpreter = Shakespeare()
         interpreter.run_play(play)
+
 
 @main.command()
 @click.argument('file')
