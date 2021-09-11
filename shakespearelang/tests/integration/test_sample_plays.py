08473f08deef0fa4b1502b79cb2667eabed159e7
Zeb Burke-Conte <zebburkeconte@gmail.com>
2021-09-10 19:07:20 -0700

    Refactor sample plays test file

--- shakespearelang/tests/integration/test_sample_plays.py
+++ shakespearelang/tests/integration/test_sample_plays.py
@@ -3,63 +3,43 @@ from io import StringIO
 from pathlib import Path
 
 def test_runs_hi(capsys):
-    path = Path(__file__).parent / "sample_plays/hi.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == 'HI\n'
-    assert captured.err == ''
+    run_sample_play('hi.spl')
+
+    assert_output(capsys, 'HI\n')
 
 def test_runs_hello_world(capsys):
-    path = Path(__file__).parent / "sample_plays/hello_world.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == 'Hello World!\n'
-    assert captured.err == ''
+    run_sample_play('hello_world.spl')
+
+    assert_output(capsys, 'Hello World!\n')
 
 def test_runs_catch(capsys):
-    path = Path(__file__).parent / "sample_plays/catch.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == 'CATCH'
-    assert captured.err == ''
+    run_sample_play('catch.spl')
+
+    assert_output(capsys, 'CATCH')
 
 def test_runs_echo(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', StringIO('m123cfoobar123'))
-    path = Path(__file__).parent / "sample_plays/echo.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == 'm123c'
-    assert captured.err == ''
+    set_input(monkeypatch, 'm123cfoobar123')
+    run_sample_play('echo.spl')
+
+    assert_output(capsys, 'm123c')
 
 def test_runs_primes(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', StringIO('20'))
-    path = Path(__file__).parent / "sample_plays/primes.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == '>2\n3\n5\n7\n11\n13\n17\n19\n'
-    assert captured.err == ''
+    set_input(monkeypatch, '20')
+    run_sample_play('primes.spl')
+
+    assert_output(capsys, '>2\n3\n5\n7\n11\n13\n17\n19\n')
 
 def test_runs_reverse(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', StringIO('first\nsecond\nthird'))
-    path = Path(__file__).parent / "sample_plays/reverse.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == '\ndriht\ndnoces\ntsrif'
-    assert captured.err == ''
+    set_input(monkeypatch, 'first\nsecond\nthird')
+    run_sample_play('reverse.spl')
+
+    assert_output(capsys, '\ndriht\ndnoces\ntsrif')
 
 def test_runs_sierpinski(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', StringIO('4'))
-    path = Path(__file__).parent / "sample_plays/sierpinski.spl"
-    with path.open() as f:
-        Shakespeare(f.read()).run()
-    captured = capsys.readouterr()
-    assert captured.out == """\
+    set_input(monkeypatch, '4')
+    run_sample_play('sierpinski.spl')
+
+    assert_output(capsys, """\
 >               *               \n\
               * *              \n\
              *   *             \n\
@@ -77,4 +57,17 @@ def test_runs_sierpinski(monkeypatch, capsys):
  *   *   *   *   *   *   *   * \n\
 * * * * * * * * * * * * * * * *\n\
 """
-    assert captured.err == ''
+    )
+
+def assert_output(capsys, output, stderr=''):
+    captured = capsys.readouterr()
+    assert captured.out == output
+    assert captured.err == stderr
+
+def set_input(monkeypatch, input):
+    monkeypatch.setattr('sys.stdin', StringIO(input))
+
+def run_sample_play(filename):
+    path = Path(__file__).parent / f'sample_plays/{filename}'
+    with path.open() as f:
+        Shakespeare(f.read()).run()
