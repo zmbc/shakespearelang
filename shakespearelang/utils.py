fea7a2c0e4f4f3da50935d03db4b9e19a0fc477c
Zeb Burke-Conte <zebburkeconte@gmail.com>
2021-09-17 14:39:46 -0700

    Use ">>" for indicating next event, fix bug with indexing

--- shakespearelang/utils.py
+++ shakespearelang/utils.py
@@ -5,15 +5,17 @@ def parseinfo_context(parseinfo, context_amount = 3):
     lines = buffer.get_lines(parseinfo.line, parseinfo.endline)
     after_context_lines = buffer.get_lines(parseinfo.endline + 1, parseinfo.endline + 1 + context_amount)
 
-    lines[0] = _add_char_at(lines[0], '>', buffer.poscol(parseinfo.pos))
-    lines[-1] = _add_char_at_before_whitespace(lines[-1], '<', buffer.poscol(parseinfo.endpos) + 1)
+    # Must insert later characters first; if you start with earlier characters, they change
+    # the indices for later inserts.
+    lines[-1] = _add_str_at_before_whitespace(lines[-1], '<<', buffer.poscol(parseinfo.endpos))
+    lines[0] = _add_str_at(lines[0], '>>', buffer.poscol(parseinfo.pos))
 
     return "".join(before_context_lines + lines + after_context_lines)
 
-def _add_char_at_before_whitespace(string, character, index):
+def _add_str_at_before_whitespace(string, character, index):
     while string[index - 1].isspace():
         index = index - 1
-    return _add_char_at(string, character, index)
+    return _add_str_at(string, character, index)
 
-def _add_char_at(string, character, index):
+def _add_str_at(string, character, index):
     return string[:index] + character + string[index:]
