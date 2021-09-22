71178feae4954a52f7be55c6ef26610519d8a43c
Zeb Burke-Conte <zebburkeconte@gmail.com>
2021-09-22 16:44:51 -0700

    Fix bug with parseinfo context

--- shakespearelang/utils.py
+++ shakespearelang/utils.py
@@ -1,16 +1,8 @@
 def parseinfo_context(parseinfo, context_amount = 3):
-    buffer = parseinfo.buffer
-    context_start_line = max(parseinfo.line - 1 - context_amount, 0)
-    before_context_lines = buffer.get_lines(context_start_line, parseinfo.line - 1)
-    lines = buffer.get_lines(parseinfo.line, parseinfo.endline)
-    after_context_lines = buffer.get_lines(parseinfo.endline + 1, parseinfo.endline + 1 + context_amount)
-
-    # Must insert later characters first; if you start with earlier characters, they change
-    # the indices for later inserts.
-    lines[-1] = _add_str_at_before_whitespace(lines[-1], '<<', buffer.poscol(parseinfo.endpos))
-    lines[0] = _add_str_at(lines[0], '>>', buffer.poscol(parseinfo.pos))
-
-    return "".join(before_context_lines + lines + after_context_lines)
+    before_context_lines = _before_context_lines(parseinfo, context_amount)
+    parsed_item_lines = _highlighted_source_text(parseinfo)
+    after_context_lines = _after_context_lines(parseinfo, context_amount)
+    return "".join(before_context_lines + parsed_item_lines + after_context_lines)
 
 def _add_str_at_before_whitespace(string, character, index):
     while string[index - 1].isspace():
@@ -19,3 +11,26 @@ def _add_str_at_before_whitespace(string, character, index):
 
 def _add_str_at(string, character, index):
     return string[:index] + character + string[index:]
+
+def _highlighted_source_text(parseinfo):
+    buffer = parseinfo.buffer
+    number_of_lines = (parseinfo.endline - parseinfo.line) + 1
+    lines = buffer.get_lines(parseinfo.line, parseinfo.endline)
+
+    end_column = buffer.poscol(parseinfo.endpos)
+    if len(lines) < number_of_lines:
+        end_column = len(lines[-1])
+
+    # Must insert later characters first; if you start with earlier characters, they change
+    # the indices for later inserts.
+    lines[-1] = _add_str_at_before_whitespace(lines[-1], '<<', end_column)
+    lines[0] = _add_str_at(lines[0], '>>', buffer.poscol(parseinfo.pos))
+
+    return lines
+
+def _before_context_lines(parseinfo, context_amount = 3):
+    context_start_line = max(parseinfo.line - 1 - context_amount, 0)
+    return parseinfo.buffer.get_lines(context_start_line, parseinfo.line - 1)
+
+def _after_context_lines(parseinfo, context_amount = 3):
+    return parseinfo.buffer.get_lines(parseinfo.endline + 1, parseinfo.endline + 1 + context_amount)
